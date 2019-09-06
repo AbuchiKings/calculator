fb05411832b793721f7f14f5c1eb5649c1e156fa
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-09-06 22:17:45 +0100

    Fixed user entry of ivalid successive values

--- script.js
+++ script.js
@@ -8,19 +8,23 @@
     const updateNum = (event) => {
         let num = event.target.id;
         let value = display.value;
-        let prevValue = value.charAt(value.length - 1);    
+        let prevValue = value.charAt(value.length - 1);
 
         if (event.target !== event.currentTarget || event.target.id !== "") {
             if (num == '.') {
                 if (value.indexOf('.') !== -1 ||
-                (prevValue && operators.includes(prevValue))) {
+                    (prevValue && operators.includes(prevValue))) {
                     ;
                 } else {
                     display.setSelectionRange(0, 0);
                     display.value += num;
 
                 }
-            } else {
+            } else if (operators.includes(num) && operators.includes(prevValue)) {
+                return;
+            }
+
+            else {
                 display.setSelectionRange(0, 0);
                 display.value += num;
             }
@@ -32,7 +36,8 @@
             display.value = '';
 
         } else if (event.target.classList.contains('eval')) {
-            if (value.length > 1) {
+            if (operators.includes(prevValue)) { return; }
+            else if (value.length > 1) {
                 result = eval(value) * 1e6
                 result = Math.round(result, 6);
                 result = result / 1e6;

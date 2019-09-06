41b35835fbc6e89c5193e51b674892afe56fa995
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-09-06 17:57:30 +0100

    Added function to update input

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-cd calculator
+
+const display = document.querySelector('#display');
+const equals = document.querySelector('.eval');
+const del = document.querySelector('.del');
+const clear = document.querySelector('.clear');
+const td = document.querySelectorAll('.calc')
+
+let digits = display.value;
+
+
+const updateNum = () => {
+    let num = event.target.id;
+    if (event.target !== event.currentTarget || event.target.id !== "") {
+        if (num == '.') {
+            if (display.value.indexOf('.') !== -1) {
+                ;
+            } else {
+                display.value += num;
+            }
+        } else {
+            display.value += num;
+        }
+    }
+};
+td.forEach(cell => {
+    cell.addEventListener('click', updateNum)
+});

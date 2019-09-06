9ccfa4f435d06aa7efd9d79fa79e165b3ed3b1ab
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-09-06 21:12:58 +0100

    Added script file

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-
-const display = document.querySelector('#display');
-const equals = document.querySelector('.eval');
-const del = document.querySelector('.del');
-const clear = document.querySelector('.clear');
-const td = document.querySelectorAll('.calc')
-
-let digits = display.value;
+(function () {
+    const display = document.querySelector('#display');
+    const td = document.querySelectorAll('.calc');
+    const allowed_keys = ['Enter', 'Backspace', 'Shift', 'Delete', '/', '+', '*', '-'];
+    const operators = ['/', '+', '*', '-'];
 
 
-const updateNum = () => {
-    let num = event.target.id;
-    if (event.target !== event.currentTarget || event.target.id !== "") {
-        if (num == '.') {
-            if (display.value.indexOf('.') !== -1) {
-                ;
+    const updateNum = (event) => {
+        let num = event.target.id;
+        let value = display.value;
+        let prevValue = value.charAt(value.length - 1);    
+
+        if (event.target !== event.currentTarget || event.target.id !== "") {
+            if (num == '.') {
+                if (value.indexOf('.') !== -1 ||
+                (prevValue && operators.includes(prevValue))) {
+                    ;
+                } else {
+                    display.setSelectionRange(0, 0);
+                    display.value += num;
+
+                }
             } else {
+                display.setSelectionRange(0, 0);
                 display.value += num;
             }
-        } else {
-            display.value += num;
+
+        } else if (event.target.classList.contains('del')) {
+            display.value = value.slice(0, value.length - 1);
+
+        } else if (event.target.classList.contains('clear')) {
+            display.value = '';
+
+        } else if (event.target.classList.contains('eval')) {
+            if (value.length > 1) {
+                result = eval(value) * 1e6
+                result = Math.round(result, 6);
+                result = result / 1e6;
+                display.value = result;
+            }
         }
-    }
-};
-td.forEach(cell => {
-    cell.addEventListener('click', updateNum)
-});
+
+
+    };
+
+
+    td.forEach(cell => {
+        cell.addEventListener('click', updateNum)
+    });
+
+
+    display.addEventListener('keydown', function (event) {
+        const allowed_key = allowed_keys.includes(event.key);
+        if ((event.key >= '0' && event.key <= '9') || allowed_key) {
+        }
+        else {
+            event.preventDefault();
+        }
+    });
+})();
